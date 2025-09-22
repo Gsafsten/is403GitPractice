00dc39f926ed6da7ade0f010d0415a3e8c44f6b3
Gsafsten <Garrett.safsten@gmail.com>
2025-09-22 08:49:55 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Interactive Pet Buttons
+function petMe(petName) {
+  alert(`You gave ${petName} some love! 🐾 They are so happy!`);
+}
+
+// Fun adopt button effect
+document.getElementById("adoptBtn").addEventListener("click", () => {
+  const pets = ["puppy 🐶", "kitten 🐱", "hamster 🐹", "parrot 🦜"];
+  const randomPet = pets[Math.floor(Math.random() * pets.length)];
+  alert(`Congrats! You’ve adopted a ${randomPet}! 🎉`);
+});
